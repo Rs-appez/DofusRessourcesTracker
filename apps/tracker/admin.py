ee76705d5ca38a1f8a934143151aec94cbbf5734
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-01-03 21:40:04 +0100

    add save_as

--- apps/tracker/admin.py
+++ apps/tracker/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Resource, RessourceValue, BuyIn, SellOut
 
-admin.site.register(Resource)
 admin.site.register(RessourceValue)
 admin.site.register(BuyIn)
 admin.site.register(SellOut)
+
+
+@admin.register(Resource)
+class ResourceAdmin(admin.ModelAdmin):
+    save_as = True
