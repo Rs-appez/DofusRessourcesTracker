79bb11005df529898a76511e8c885def3ee1c0d4
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-01-04 02:06:53 +0100

    add wanted detail view (wip)

--- apps/tracker/urls.py
+++ apps/tracker/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.dashboard_view, name="dashboard"),
     path("create-wanted/", views.create_wanted_view, name="create_wanted"),
     path("wanted/", views.wanted_view, name="wanted"),
+    path("wanted/<int:wanted_id>/", views.wanted_detail_view, name="wanted_detail"),
 ]
--- apps/tracker/views.py
+++ apps/tracker/views.py
@@ -1,14 +1,35 @@
-from apps.tracker.models import ResourceImage, Resource
-from django.shortcuts import redirect
-from django.shortcuts import render
+from django.http.response import Http404
+from django.shortcuts import get_object_or_404, redirect, render
+
+from apps.tracker.models import Resource, ResourceImage, ResourceType
 
 
 def dashboard_view(request):
     return render(request, "tracker/dashboard.html")
 
+
 def wanted_view(request):
-    wanteds = Resource.objects.filter(resource_type=5)
+    wanteds = Resource.objects.filter(resource_type=ResourceType.WANTED.value)
     return render(request, "tracker/wanted.html", {"wanteds": wanteds})
+
+
+def wanted_detail_view(request, wanted_id):
+    wanted = get_object_or_404(Resource, id=wanted_id)
+    if wanted.resource_type != ResourceType.WANTED.value:
+        raise Http404("Wanted not found")
+
+    card = Resource.objects.filter(
+        use_in=wanted, resource_type=ResourceType.CARD.value
+    ).first()
+
+    fragments = Resource.objects.filter(
+        use_in=card, resource_type=ResourceType.CARD.value
+    ).order_by("name")
+
+    return render(
+        request, "tracker/wanted_detail.html", {"wanted": wanted, "card": card, "fragments": fragments}
+    )
+
 
 def create_wanted_view(request):
     if request.method == "GET":
@@ -26,11 +47,13 @@ def create_wanted_view(request):
     if created:
         ress_img.image.save(image.name, image)
 
-    ress = Resource.objects.create(name=name, image=ress_img, resource_type=5)
+    ress = Resource.objects.create(
+        name=name, image=ress_img, resource_type=ResourceType.WANTED.value
+    )
     card = Resource.objects.create(
         name=f"Carte de {wanted_name}",
         image=card_img,
-        resource_type=6,
+        resource_type=ResourceType.CARD.value,
     )
     card.use_in.add(ress)
 
@@ -38,7 +61,7 @@ def create_wanted_view(request):
         Resource.objects.create(
             name=f"Fragment de carte {wanted_name} {i + 1}/{nb}",
             image=frag_img,
-            resource_type=6,
+            resource_type=ResourceType.CARD.value,
         ).use_in.add(card)
 
     return redirect("create_wanted")
