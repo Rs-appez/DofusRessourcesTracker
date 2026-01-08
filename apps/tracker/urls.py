f058c5d8768a19fb674c5d986e55df97208b7888
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-01-08 04:16:08 +0100

    feat(tracker): add buy all cards API endpoint
    - Imported `views_api` in `urls.py` and `__init__.py`.
    - Added API route `api/buy-all/<wanted_id>/` for buying all cards.
    - Updated urlpatterns and view imports for better API structure.

--- apps/tracker/urls.py
+++ apps/tracker/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
-from .views import views, views_parital
+from .views import views, views_parital, views_api
 
 app_name = "tracker"
 
 urlpatterns = [
     path("", views.dashboard_view, name="dashboard"),
+    # Page views
     path("create-wanted/", views.create_wanted_view, name="create_wanted"),
     path("wanted/", views.wanted_view, name="wanted"),
+    # API views
+    path(
+        "api/buy-all/<wanted_id>/", views_api.buy_all_cards_view, name="buy_all_cards"
+    ),
     # Partial views (htmx)
     path(
         "wanted/<int:wanted_id>/",
--- apps/tracker/views/__init__.py
+++ apps/tracker/views/__init__.py
@@ -1,2 +1,3 @@
 from .views import *
+from .views_api import *
 from .views_parital import *
