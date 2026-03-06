5593f0caa9209d64341918e2e9d7e34cdb2ab2e2
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-03-06 17:22:42 +0100

    feat(settings): switch DB to PostgreSQL for production
    Replaced the default SQLite configuration with PostgreSQL settings using
    environment variables. This change prepares the project for production
    deployment and improves scalability. The previous SQLite config is
    commented out for reference.

--- dofRessTrack/settings.py
+++ dofRessTrack/settings.py
@@ -67,8 +67,17 @@ WSGI_APPLICATION = "dofRessTrack.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+    # if DEBUG
+    # else {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("POSTGRES_HOST"),
+        "PORT": env("POSTGRES_PORT"),
     }
 }
 
