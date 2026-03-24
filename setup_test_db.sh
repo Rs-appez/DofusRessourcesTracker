47e362c3d77f95b0f197ef2e11d7586ab971fd2f
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-03-24 22:06:25 +0100

    feat(scripts): add echo prompts to test DB setup script
    Added informative echo statements to the setup_test_db.sh script to
    indicate when the database is being dumped from the remote server and
    restored to the local server. This improves script clarity and user
    experience during test database setup.

--- setup_test_db.sh
+++ setup_test_db.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+echo "Dumping the database from the remote server..."
+echo "Password for the prod db"
 pg_dump -U appez -h db.appez.cafe -p 54321 -F c -d dofresstrack -f dofresstrack_backup.dump
+echo "Restoring the database to the local server..."
+echo "Password for the local db"
 pg_restore -U appez -h localhost -p 5432 -O -x -c -d dofresstrack -1 dofresstrack_backup.dump
